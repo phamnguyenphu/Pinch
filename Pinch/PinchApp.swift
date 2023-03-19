3fd412173fe8f1e95cb1efc498903244a4e45c1a
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-03-19 14:53:08 +0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Pham Nguyen Phu on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
