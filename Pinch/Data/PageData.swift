625fc7479f30d2092aade9b1eb9b6b5adc76b5bc
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-03-22 15:04:26 +0700

    SwiftUI page Thumbnails

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Pham Nguyen Phu on 22/03/2023.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 0, imageName: "magazine-front-cover"),
+    Page(id: 1, imageName: "magazine-back-cover")
+]
